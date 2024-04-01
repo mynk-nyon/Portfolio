2e12f4e47a7eee8bfb80aed72a4f98c48a414f93
Mayank Shukla <77397565+mynk-nyon@users.noreply.github.com>
2024-04-01 12:34:05 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -45,8 +45,8 @@ window.addEventListener('scroll', scrollActive)
 const sr = ScrollReveal({
     origin: 'top',
     distance: '60px',
-    duration: 20000,
-    delay: 200,
+    duration: 2000,
+    delay: 20000,
 //     reset: true
 });
 
